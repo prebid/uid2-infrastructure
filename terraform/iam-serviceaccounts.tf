1f609e417cccacd0e623c7cf2215c1abe7bfa31b
Ivan Gusev <ivan.gusev@openx.org>
2021-03-01 15:56:59 -0800

    Grant permissions to terraform to use compute serviceaccount

--- terraform/iam-serviceaccounts.tf
+++ terraform/iam-serviceaccounts.tf
@@ -1,4 +1,14 @@
+# Do not use default compute service account as it has excessive permissions
 resource "google_service_account" "compute" {
   account_id   = "uid2-compute"
   display_name = "Dummy service account for GKE compute nodes"
+}
+
+resource "google_service_account_iam_binding" "admin-account-iam" {
+  service_account_id = google_service_account.compute.name
+  role               = "roles/iam.serviceAccountUser"
+
+  members = [
+    "serviceAcccount:${google_service_account.terraform.email}",
+  ]
 }
