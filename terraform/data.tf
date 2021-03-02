33a06bbcbec2b96715dd96a7eac246c03650cbbb
Ivan Gusev <ivan.gusev@openx.org>
2021-03-02 12:38:13 -0800

    Forgotten commit

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,6 +1,9 @@
 data "google_compute_regions" "available" {
 }
 
+data "google_client_config" "provider" {
+}
+
 locals {
   # Creative way to obtain project id without enabling Cloud Resource Manager API
   project_id = split(".", split("@", google_service_account.compute.email)[1])[0]
