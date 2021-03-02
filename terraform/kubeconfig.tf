cb471b9088d75f8eda06ddccb386e6865003fa00
Ivan Gusev <ivan.gusev@openx.org>
2021-03-02 12:36:09 -0800

    Render kubeconfig files for helm to use

---
+++ terraform/kubeconfig.tf
@@ -0,0 +1,16 @@
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/templates/kubeconfig.yaml.tmpl")
+  for_each    = google_container_cluster.primary
+  vars = {
+    token = data.google_client_config.provider.access_token
+    endpoint  = each.value.endpoint
+    cluster_ca_certificate = each.value.master_auth[0].cluster_ca_certificate
+    name = each.value.name
+  }
+}
+
+resource "local_file" "kubeconfig" {
+    for_each = data.template_file.kubeconfig
+    content     = each.value.rendered
+    filename = "${path.module}/kubecontext-${each.key}.yaml"
+}
--- terraform/output.tf
+++
@@ -1,4 +0,0 @@
-output "environment" {
-  description = "Delivery team VPC network"
-  value       = var.environment
-}
