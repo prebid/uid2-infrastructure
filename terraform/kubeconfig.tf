2b69e34b0c6b2429a4941235ae95b26406325286
Ivan Gusev <ivan.gusev@openx.org>
2021-03-03 16:17:57 -0800

    Replace template_file resource with templatefile() function

--- terraform/kubeconfig.tf
+++ terraform/kubeconfig.tf
@@ -1,17 +1,6 @@
-data "template_file" "kubeconfig" {
-  template = file("${path.module}/templates/kubeconfig.yaml.tmpl")
-  for_each    = merge(google_container_cluster.primary, google_container_cluster.mission_control)
-  vars = {
-    token = data.google_client_config.provider.access_token
-    endpoint  = each.value.endpoint
-    cluster_ca_certificate = each.value.master_auth[0].cluster_ca_certificate
-    name = each.value.name
-  }
-}
-
 resource "local_file" "kubeconfig" {
-    for_each = data.template_file.kubeconfig
-    sensitive_content     = each.value.rendered
-    filename = "${path.module}/kubecontext-${each.key}.yaml"
-    file_permission = "0600"
+  for_each          = merge(google_container_cluster.primary, google_container_cluster.mission_control)
+  sensitive_content = templatefile("${path.module}/templates/kubeconfig.yaml.tmpl", { token = data.google_client_config.provider.access_token, endpoint = each.value.endpoint, cluster_ca_certificate = each.value.master_auth[0].cluster_ca_certificate, name = each.value.name })
+  filename          = "${path.module}/kubecontext-${each.key}.yaml"
+  file_permission   = "0600"
 }
