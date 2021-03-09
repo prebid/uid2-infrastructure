1d237b31705087ca0f47f450443bd10a13d0347f
Ivan Gusev <ivan.gusev@openx.org>
2021-03-09 15:36:40 -0800

    Remove unused module variable

--- terraform/iam-workload-identities.tf
+++ terraform/iam-workload-identities.tf
@@ -3,5 +3,4 @@ module "workload_identity_autoneg-system" {
   name                = "autoneg-system"
   namespace           = "autoneg-system"
   project             = local.project_id
-  use_existing_k8s_sa = true # Do not create kubernetes SA, fleet will deploy yaml
 }
