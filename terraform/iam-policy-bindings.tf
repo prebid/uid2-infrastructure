db5292ebfe78c3d2545e3695ac619f79918bdd34
Ivan Gusev <ivan.gusev@openx.org>
2021-03-09 15:35:02 -0800

    Provision access for autoneg SA

---
+++ terraform/iam-policy-bindings.tf
@@ -0,0 +1,8 @@
+resource "google_project_iam_binding" "project" {
+  project = local.project_id
+  role    = google_project_iam_custom_role.autoneg.id
+
+  members = [
+    module.workload_identity_autoneg-system.gcp_service_account_fqn,
+  ]
+}
