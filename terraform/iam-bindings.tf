ed5df1ea727afc2f5518b267f8139f44a7243ac2
Ivan Gusev <ivan.gusev@openx.org>
2021-03-10 09:58:13 -0800

    Give up on restricted terraform sa and grant permissions on the project level

--- terraform/iam-bindings.tf
+++ terraform/iam-bindings.tf
@@ -1,5 +1,4 @@
-resource "google_iap_web_backend_service_iam_binding" "autoneg" {
-  web_backend_service = google_compute_backend_service.uid2_server.name
+resource "google_project_iam_binding" "autoneg" {
   role = google_project_iam_custom_role.autoneg.name
   members = [
     module.workload_identity_autoneg-system.gcp_service_account_fqn
