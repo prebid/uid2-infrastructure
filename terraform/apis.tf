08d48c2141e9358e027371dde4e031a936eeb1e1
Ivan Gusev <ivan.gusev@openx.org>
2021-03-10 10:59:02 -0800

    enable google domains api

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -2,6 +2,7 @@ resource "google_project_service" "apis" {
   for_each = toset([
     "servicemanagement.googleapis.com",
     "cloudresourcemanager.googleapis.com",
+    "domains.googleapis.com",
   ])
 
   service = each.key
