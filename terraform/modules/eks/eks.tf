60c1f708f12eb822353fcde1af9218e56cf27b9a
Ivan Gusev <ivan.gusev@openx.org>
2021-03-25 16:42:32 -0700

    Add AWS-LB crds to EKS and create iamserviceaccount role

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -11,6 +11,11 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
+provider "kubernetes-alpha" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
 provider "helm" {
   kubernetes {
