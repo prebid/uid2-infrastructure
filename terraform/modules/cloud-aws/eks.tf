939e7196b610452bde93a36660caf348e6549dc8
Ivan Gusev <ivan.gusev@openx.org>
2021-03-31 15:56:38 -0700

    Generate AWS providers for all regions so resource cleanup is possible after region is removed

--- terraform/modules/cloud-aws/eks.tf
+++ terraform/modules/cloud-aws/eks.tf
@@ -1,5 +1,6 @@
+# Generare providers for all regions so we can delete resources when region is removed
 resource "local_file" "providers" {
-    content     = templatefile("${path.module}/templates/providers.tf.tpl", { regions = var.regions })
+    content     = templatefile("${path.module}/templates/providers.tf.tpl", { regions = local.aws_regions })
     filename = "${path.root}/../stage2/providers_generated.tf"
     file_permission = "0644"
 }
