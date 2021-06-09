3e530722119c9bd0472010abc3ce1d20e30801fa
Ivan Gusev <ivan.gusev@openx.org>
2021-06-09 16:06:02 -0700

    Move iap_support_email default up to the top

--- terraform/stage1/variables.tf
+++ terraform/stage1/variables.tf
@@ -7,3 +7,8 @@ variable "regions" {
   type        = list
   description = "List of regions, can be AWS and GCP regions, Azure in the future"
 }
+
+variable "iap_support_email" {
+  description = "Support email address used to establish IAP brand"
+  default = "igusev@prebid.org"
+}
